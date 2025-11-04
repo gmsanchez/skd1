b6cc04f1b0690fe247aae225c08df2f68d5ffb97
Guido Sanchez <gsanchez@sinc.unl.edu.ar>
2025-11-04 11:50:45 -0300

    Upload skd1_localization package.

---
+++ skd1_localization/launch/ekf_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    ekf_node = Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters = [{'use_sim_time': use_sim_time},
+                          PathJoinSubstitution([FindPackageShare('skd1_localization'), 'config', 'ekf.yaml'])],
+            remappings=[("odometry/filtered", "odometry/filtered"),
+                            ("imu/data", "phidgets_373223/imu/data")],
+           )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='False',
+            description='Use simulation (Gazebo) clock if true'),
+        ekf_node,
+])
